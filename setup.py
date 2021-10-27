2aa7d64d4539e2304de452c3d05763e4f31f64bd
damilu <69503954+damilu1@users.noreply.github.com>
2021-10-27 21:10:38 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+ 
+setup(
+    name="pip_damilu",
+    version="0.0.1",
+    license="MIT Licence",
+ 
+    url="https://github.com/wshzd/NLP/new/master/pip_package/pip_damilu",
+    author="damilu",
+    author_email="w-b-h-s-d@163.com",
+ 
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
